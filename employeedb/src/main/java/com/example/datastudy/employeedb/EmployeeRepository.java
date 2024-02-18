fc5abe5b21ee128e0d45e5a1ab4c579035c19017
Prathap Dhanasekaran <prathapprof@gmail.com>
2024-02-18 20:35:04 +0100

    Database created with id autogenerated

--- employeedb/src/main/java/com/example/datastudy/employeedb/EmployeeRepository.java
+++ employeedb/src/main/java/com/example/datastudy/employeedb/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.example.datastudy.employeedb;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    List<Employee> findEmployeeByLastNameContaining(String string);
 
 }
--- employeedb/src/main/java/com/example/datastudy/employeedb/EmployeedbApplication.java
+++ employeedb/src/main/java/com/example/datastudy/employeedb/EmployeedbApplication.java
@@ -1,13 +1,32 @@
 package com.example.datastudy.employeedb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeedbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeedbApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(EmployeeRepository employeeRepository)
+	{
+		return (args -> {
+			insert(employeeRepository);
+		    System.out.println(employeeRepository.findAll());
+		});
+	}
+
+	private void insert(EmployeeRepository employeeRepository)
+	{
+		employeeRepository.save(new Employee("John","Christopher"));
+		employeeRepository.save(new Employee("Adam","Jones"));
+		employeeRepository.save(new Employee("Will","Robinson"));
+		employeeRepository.save(new Employee("Matelda","Angel"));
 	}
 
 }
